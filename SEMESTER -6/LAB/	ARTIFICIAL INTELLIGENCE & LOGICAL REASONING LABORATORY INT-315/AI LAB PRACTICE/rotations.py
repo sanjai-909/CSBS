d542d8eec81dfcb833119e69f99f4cb5a3dd0b72
SanJai SK <132538008+thesanjai@users.noreply.github.com>
2025-01-27 14:33:50 +0530

    updated denominatios code version 2

--- SEMESTER -6/LAB/	ARTIFICIAL INTELLIGENCE & LOGICAL REASONING LABORATORY INT-315/AI LAB PRACTICE/rotations.py
+++ SEMESTER -6/LAB/	ARTIFICIAL INTELLIGENCE & LOGICAL REASONING LABORATORY INT-315/AI LAB PRACTICE/rotations.py
@@ -3,16 +3,18 @@ Denominations = [2000, 500, 200, 100, 50, 20, 10, 5, 2, 1]
 k = int(input("Enter the count:"))
 k %= len(Denominations)
 
-print(f"Original Array : {Denominations}")
-
-Denominations = Denominations[::-1]
-
 left_rotation = Denominations.copy()
+left_rotation.reverse()
 left_rotation[:k] = left_rotation[:k][::-1]
 left_rotation[k:] = left_rotation[k:][::-1]
-print(f"Left Rotation : {left_rotation}")
 
 right_rotation = Denominations.copy()
+right_rotation.reverse()
 right_rotation[:-k] = right_rotation[:-k][::-1]
 right_rotation[-k:] = right_rotation[-k:][::-1]
+
+print(f"Original Array : {Denominations}")
+print(f"Left Rotation : {left_rotation}")
 print(f"Right Rotation : {right_rotation}")
+
+
